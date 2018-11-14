6b05c71d2c57b56d10d8cddf0c731df51dadd4ee
Param Thakkar <pht0104@gmail.com>
2018-11-13 21:06:01 -0500

    web: param.me/kevlar

--- redirect.js
+++ redirect.js
@@ -1,5 +1,6 @@
 var REDIRECTS = {
   'website': 'https://www.param.me',
   'hht': 'https://www.1ht.co',
-  'mp3': 'https://www.param.me/kevlar/animation.mp3'
+  'mp3': 'https://www.param.me/kevlar/animation.mp3',
+   'web': 'https://www.param.me/kevlar'
 }
