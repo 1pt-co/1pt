9a423a85d3d7bb304463abdfdbdeed7d1f789f0f
Param Thakkar <pht0104@gmail.com>
2018-11-13 21:15:47 -0500

    change mp3 to mp4

--- redirect.js
+++ redirect.js
@@ -1,6 +1,6 @@
 var REDIRECTS = {
   'website': 'https://www.param.me',
   'hht': 'https://www.1ht.co',
-  'mp3': 'https://www.param.me/kevlar/animation.mp3',
+  'mp4': 'https://www.param.me/kevlar/animation.mp4',
    'web': 'https://www.param.me/kevlar'
 }
