75b71a06da5dbc6c946335525484d5e822c27bca
Param Thakkar <pht0104@gmail.com>
2018-12-12 07:43:19 -0500

    Fix redirect from 404

--- redirect.js
+++ redirect.js
@@ -12,6 +12,7 @@ window.onload = function(){
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       REDIRECTS.[shortURL] = this.responseText;
+      window.location = REDIRECTS.[window.location.href.split('1pt.co/')[1]];
     }
   };
   xhttp.open("GET", "https://thakkaha.dev.fast.sheridanc.on.ca/pme/1pt/retrieve-url-from-db.php?url=" + document.getElementById("url").value, true);
