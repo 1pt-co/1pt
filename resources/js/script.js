e7062dab255ae556968ac0590e1321673e0f26ea
Param Thakkar <pht0104@gmail.com>
2019-04-20 15:09:23 -0400

    Refactor code Closes #5

---
+++ resources/js/script.js
@@ -0,0 +1,134 @@
+if(screen.width > 1350){
+  new TypeIt('#typeit', {
+    speed: 75,
+    breakLines: false,
+    autoStart: false
+  })
+  .type(" &#124; Dynamic URL Shortener")
+} else {
+  new TypeIt('#typeit', {
+    speed: 60,
+    breakLines: false,
+    autoStart: false
+  })
+  .pause(300)
+  .type(" &#124; URL Shortener")
+  .pause(1000)
+  .delete(13)
+  .pause(100)
+  .type("Dynamic Linking");
+}
+
+function positionError(){
+  var error = document.getElementById("error");
+  var left = (window.innerWidth - error.offsetWidth)/2;
+  error.style.left = left + "px";
+}
+
+window.onload = function(){
+  positionError()
+  window.setTimeout(function(){
+    document.getElementById("arrow").style.visibility = "visible";
+      document.getElementById("arrow").classList = "animated bounce";
+  }, 2500);
+
+}
+window.onresize = function(){positionError()}
+
+function removeSpaces(url){
+  var url = document.getElementById("url");
+  url.value = url.value.replace(/ /g, "");
+}
+
+var buttonShown = false;
+var txt = document.getElementById("url");
+var btn = document.getElementById("btn");
+
+function showButton(){
+  if(buttonShown == true && txt.value == ""){
+    txt.style.width = "80vw";
+    btn.style.width = "0px";
+    btn.style.color = "#424242";
+    btn.style.paddingLeft = "0";
+    btn.style.paddingRight = "0";
+    buttonShown = false;
+  } else if (txt.value !== "") {
+    txt.style.width = "70vw";
+    btn.style.padding = "10px";
+    btn.style.width = "10vw";
+    btn.style.color = "white";
+    buttonShown = true;
+  }
+}
+
+var link;
+var url = document.getElementById("url").value;
+
+function submit(url){
+  //alert(encodeURI(url));
+  var pattern = new RegExp(/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/g);
+  var isURL = pattern.test(url);
+
+  if(isURL){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        link = "https://1pt.co/" + this.responseText;
+        document.getElementById("short-url").value = link.slice(8);
+        document.getElementById("qr-code-link").href = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + link;
+        document.getElementById("qr-code").src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + link;
+      }
+    };
+    xhttp.open("GET", "https://thakkaha.dev.fast.sheridanc.on.ca/pme/1pt/add-url-to-db.php?url=" + txt.value + "&de=" + document.getElementById("desktop").value + "&mo=" + document.getElementById("mobile").value + "&ap=" + document.getElementById("apple").value + "&an=" + document.getElementById("android").value + "&cu=" + document.getElementById("custom").value.replace("https://1pt.co/"), true);
+    xhttp.send();
+
+    txt.classList = "animated bounceOutLeft";
+    btn.classList = "btn animated bounceOutLeft";
+
+    document.getElementById("short-url").style.display = "inline-block";
+    document.getElementById("short-url").classList = "animated delay-500 bounceInRight";
+    document.getElementById("qr-code-section").style.display = "block";
+    document.getElementById("settings-section").style.display = "none";
+  } else {
+    txt.value = "";
+    showButton();
+    document.getElementById("error").style.opacity = "1";
+    document.getElementById("error").classList = "error animated shake";
+    window.setTimeout(function(){
+      document.getElementById("error").style.opacity = "0";
+      document.getElementById("error").classList = "error";
+    }, 3000)
+  }
+}
+
+window.addEventListener("scroll", function(){
+	var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
+	if(scrollTop !== 0){
+    document.getElementById("short-url").style.boxShadow = '0px 0px 0px 2px white';
+    document.getElementById('arrow').classList = 'animated fadeOutUp';
+  } else {
+    document.getElementById("short-url").style.boxShadow = 'none';
+    document.getElementById('arrow').classList = 'animated bounce';
+  }
+
+}, false);
+
+// click GO button if user presses ENTER key
+txt.addEventListener("keyup", function(event) {
+  event.preventDefault();
+  if (event.keyCode === 13 && txt.style.width === "70vw") {
+    btn.click();
+  }
+});
+
+function appendURL(obj){
+  if(obj.value.slice(0, 15) === "https://1pt.co/"){
+
+  } else if(obj.value.slice(0, 14) === "https://1pt.co"){
+    obj.value = 'https://1pt.co/';
+  } else {
+    obj.value = 'https://1pt.co/' + obj.value;
+  }
+
+  obj.value = obj.value.replace(/ /g, "");
+}
