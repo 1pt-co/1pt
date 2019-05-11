8b46bb64eddab6b6b3e474a953ecdc0e9aa394ac
Param Thakkar <pht0104@gmail.com>
2019-05-11 18:31:48 -0400

    Create API wrapper

---
+++ api/index.php
@@ -0,0 +1,41 @@
+<?php
+
+if(!empty($_GET["url"])) {
+    $long = $_GET["url"];
+    $desktop = @$_GET["desktop"];
+    $mobile = @$_GET["mobile"];
+    $apple = @$_GET["apple"];
+    $android = @$_GET["android"];
+    $custom = @$_GET["custom"];
+
+    $query = "https://thakkaha.dev.fast.sheridanc.on.ca/pme/1pt/add-url-to-db.php?url=" . $long;
+
+    if(isset($_GET['desktop'])){
+        $query = $query . "&de=" . $desktop;
+    }
+
+    if(isset($_GET['mobile'])){
+        $query = $query . "&mo=" . $mobile;
+    }
+
+    if(isset($_GET['apple'])){
+        $query = $query . "&ap=" . $apple;
+    }
+
+    if(isset($_GET['android'])){
+        $query = $query . "&an=" . $android;
+    }
+
+    if(isset($_GET['custom'])){
+        $query = $query . "&cu=" . $custom;
+    }
+
+    echo(file_get_contents($query));
+
+} else {
+    header('HTTP/1.0 400 Bad Request');
+    echo("Invalid request!");
+    exit();
+}
+
+?>
