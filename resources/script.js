dcf3b59baf6db91d4dd22e97f3732c18dc0cc816
Param Thakkar <pht0104@gmail.com>
2020-02-08 19:33:43 -0500

    Add output functionality

--- resources/script.js
+++ resources/script.js
@@ -1,11 +1,12 @@
 var input = document.getElementById("url");
-var button = document.getElementById("next");
+var next = document.getElementById("next");
+var submit = document.getElementById("submit");
 
 // Click GO button if the ENTER key is pressed
 input.addEventListener("keyup", function(event) {
     event.preventDefault();
     if (event.keyCode == 13) {
-        button.click();
+        next.click();
     }
 });
 
@@ -14,9 +15,10 @@ function showOptions() {
     if(input.value != "" && validateURL(input.value)) {
         input.disabled = true;
         input.style.width = "1200px";
+        next.style.display = "none";
+
         // Apply the top-left border-radius to all 4 sides
         input.style.borderRadius = getComputedStyle(input).borderRadius.split(" ")[0];
-        button.style.display = "none";
 
         document.getElementById("top").style.height = "300px";
         document.getElementById("options").style.display = "block";
@@ -36,9 +38,9 @@ function showError(message) {
 
 // Return true if the input could be a valid URL
 function validateURL(url) {
-    var regex = new RegExp(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi);
+    var regex = new RegExp(/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/g);
 
-    if (url.match(regex)) {
+    if (regex.test(url)) {
         return true;
     } else {
         return false;
@@ -49,3 +51,60 @@ function validateURL(url) {
 var cleave = new Cleave("#custom-url", {
     prefix: "1pt.co/",
 });
+
+function displayOutput(shortURL) {
+    qrCode = document.getElementById("qr-code");
+    output = document.getElementById("output");
+    outputWrapper = document.getElementById("output-wrapper");
+    copyBtn = document.getElementById("copy");
+
+
+    qrCode.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + "https://" + shortURL;
+    output.value = shortURL;
+
+    copyBtn.style.visibility = "visible";
+    output.style.visibility = "visible";
+
+    outputWrapper.classList.add("animated", "faster", "zoomIn");
+    copyBtn.classList.add("animated", "faster", "zoomIn");
+
+    qrCode.onload = function(){
+        qrCode.style.visibility = "visible";
+        qrCode.classList.add("animated", "faster", "zoomIn");
+    }
+}
+
+// Return short URL given a long URL
+function sendRequest(longURL, shortURL){
+    if(validateURL(longURL)){
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                shortURL = "1pt.co/" + this.responseText;
+                displayOutput(shortURL);
+            }
+        };
+
+        request = "https://thakkaha.dev.fast.sheridanc.on.ca/pme/1pt/add-url-to-db.php?url=" + longURL + "&cu=" + encodeURI(shortURL);
+        xhttp.open("GET", request, true);
+        xhttp.send();
+    } else {
+        showError("There was an error!");
+    }
+}
+
+function remove(string, toRemove) {
+
+    toRemove.forEach(function(item, index) {
+        console.log(item);
+        string = string.replace(new RegExp(item, "gi"), "");
+        console.log(index);
+    })
+
+    return string;
+}
+
+submit.onclick = function() {
+    customURL = document.getElementById("custom-url");
+    sendRequest(input.value, remove(customURL.value, ["1pt.co/", "/", "\\?"]));
+};
