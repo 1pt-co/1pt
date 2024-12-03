65962883a2f2591b0b0edb02fe1ab65c11ed1782
Param Thakkar <pht0104@gmail.com>
2024-12-02 23:14:19 -0500

    Change api endpoint

--- resources/js/script.js
+++ resources/js/script.js
@@ -99,7 +99,7 @@ function sendRequest(longURL, shortURL) {
     longURL = encodeURIComponent(longURL);
 
     request =
-      "https://csclub.uwaterloo.ca/~phthakka/1pt/addURL.php?url=" +
+      "https://thakdees.dev.fast.sheridanc.on.ca/1pt/addURL.php?url=" +
       longURL +
       "&cu=" +
       encodeURI(shortURL);
